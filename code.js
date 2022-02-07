adb0960f468216cd939f58434b13fea32c886e8c
Claudio Angrigiani <angrigianicr@gmail.com>
2022-02-06 22:54:07 -0500

    small cleanup

--- code.js
+++ code.js
@@ -1,21 +1,8 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
-    });
-}
 const annotations = [];
 for (const node of figma.currentPage.selection) {
     if ("children" in node) {
         for (let i = 0; i < node.children.length; i++) {
+            //filter only instances of components
             if (node.children[i].type == 'INSTANCE') {
                 annotations.push(node.children[i].name);
             }
--- code.ts
+++ code.ts
@@ -1,12 +1,9 @@
-async function main() {
-  await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
-}
-
 const annotations:any[] = [];
 
 for (const node of figma.currentPage.selection) {
   if ("children" in node) {
     for (let i=0; i < node.children.length; i++) {
+      //filter only instances of components
       if (node.children[i].type == 'INSTANCE') {
         annotations.push(node.children[i].name)
       }
